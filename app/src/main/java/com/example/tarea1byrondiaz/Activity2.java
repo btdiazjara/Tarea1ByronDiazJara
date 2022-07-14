d5c04fbf4e66a837387e99c796690f7873cc00c2
btdiazjara <byron.diazj@utem.cl>
2022-07-13 23:52:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tarea1byrondiaz/Activity2.java
@@ -0,0 +1,45 @@
+package com.example.tarea1byrondiaz;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Activity2 extends AppCompatActivity {
+
+	Button boton2;
+	Intent volverIntent;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.i( "Activity2", "Abriendo Activity 2");
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_2);
+
+		boton2 = findViewById(R.id.boton2);
+
+		volverIntent = new Intent(Activity2.this, com.example.tarea1byrondiaz.MainActivity.class);
+
+
+		boton2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view){
+				startActivity(volverIntent);
+				finish();
+			}
+		});
+	}
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		Log.i("Activity2", "Cerrando");
+	}
+}
---
+++ app/src/main/java/com/example/tarea1byrondiaz/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.tarea1byrondiaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+	Button boton;
+	Intent irActividad2Intent;
+	Toast irActividad2Toast;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.i( "MainActivity", "Abriendo MainActivity");
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		boton = findViewById(R.id.boton1);
+		irActividad2Intent = new Intent(MainActivity.this, Activity2.class);
+		irActividad2Toast = Toast.makeText(MainActivity.this, "Ir a la actividad 2", Toast.LENGTH_LONG);
+
+		boton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view){
+				startActivity(irActividad2Intent);
+				irActividad2Toast.show();
+			}
+		});
+	}
+}
